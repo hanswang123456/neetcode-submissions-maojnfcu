034412dc45930fd14f2efa5aa73b01cf5b8d0f68
hanswang123456 <50556568+hanswang123456@users.noreply.github.com>
2026-04-08 22:12:31 -0400

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    maxAreaOfIsland(grid) {
+        let max = 0;
+
+        let expand = function(i, j, size) {
+            if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) return size;
+            grid[i][j] = 0;
+            let left = expand(i - 1, j, size); 
+            let right = expand(i + 1, j, size); 
+            let top = expand(i, j - 1, size); 
+            let bottom = expand(i, j + 1, size); 
+
+            return left + right + top + bottom + 1;
+        }
+
+        for(let i = 0; i < grid.length; i++) {
+            for(let j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 1) {
+                    max = Math.max(max, expand(i, j, 0));
+                }
+            }
+        }
+
+        return max;
+
+    }
+}
