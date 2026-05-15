21e9ecfbc57aad0e75c53f593815482d39b8bdab
hanswang123456 <50556568+hanswang123456@users.noreply.github.com>
2026-05-14 22:37:55 -0400

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.js
@@ -0,0 +1,22 @@
+class KthLargest {
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     */
+    constructor(k, nums) {
+        this.val = nums;
+        this.val = this.val.sort((a,b) => b - a);
+        this.k = k;
+    }
+
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val) {
+        this.val.push(val);
+        this.val = this.val.sort((a,b) => b - a)
+        console.log(this.val)
+        return this.val[this.k - 1];
+    }
+}
